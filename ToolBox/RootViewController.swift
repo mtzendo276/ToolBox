5189fbe8f1e8bec710a847e2560414d73eb887c3
Victor Chen <victor.chen@mtzendo.com>
2024-01-03 22:20:14 +1300

    remove main storyboard

--- ToolBox/RootViewController.swift
+++ ToolBox/RootViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        title = "Root"
     }
 
 
